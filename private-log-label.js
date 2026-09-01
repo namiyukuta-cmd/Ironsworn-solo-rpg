563c4ceca17d8534e9e280a3ef8d1d42a603f871
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-02 08:21:29 +0900

    Remove obsolete private log label helper

--- private-log-label.js
+++
@@ -1,8 +0,0 @@
-(()=>{'use strict';
-const btn=document.getElementById('rogBtn');
-if(!btn)return;
-btn.addEventListener('click',()=>{
-  const kicker=document.getElementById('stageKicker');
-  if(kicker)kicker.textContent='LOG';
-});
-})();
